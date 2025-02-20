9e0a0d8d1def250a1e37390cccb363c121656ac8
jacoloves <5511068t@gmail.com>
2025-02-20 08:31:19 +0000

    mod: tmp.hs

--- tmp.hs
+++ tmp.hs
@@ -1,14 +1,27 @@
-solver :: String -> String -> Int
-solver s1 s2
-  | s1 == "sick" && s2 == "sick" = 1
-  | s1 == "sick" && s2 == "fine" = 2
-  | s1 == "fine" && s2 == "sick" = 3
-  | otherwise = 4
+-- Control.Monad
+-- replicateM
+import Control.Monad (replicateM)
+
+solver :: [(Int, Int)] -> String
+solver scores =
+  let (t, a) = foldl (\(t, a) (x, y) -> (t + x, a + y)) (0, 0) scores
+  in if t > a
+    then "Takahashi"
+    else if t < a
+      then "Aoki"
+      else "Draw"
 
 main :: IO ()
 main = do
-  [s1, s2] <- getStrArray
-  print $ solver s1 s2
+  n <- readLn :: IO Int
+  scores <- readDoubleIntScore n
+  putStrLn $ solver scores
+
+-- read double Int score
+readDoubleIntScore :: Int -> IO [(Int, Int)]
+readDoubleIntScore n = replicateM n $ do
+  [x, y] <- map read . words <$> getLine
+  return (x, y)
 
 -- read String Array
 getStrArray :: IO [String]
