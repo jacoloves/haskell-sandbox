695d2d158ed40bfa69896c719fadd23771793d70
jacoloves <5511068t@gmail.com>
2025-02-25 01:08:22 +0000

    20250225

---
+++ abc394a.hs
@@ -0,0 +1,29 @@
+-- Control.Monad
+-- replicateM
+import Control.Monad (replicateM)
+
+solver :: String -> String
+solver s = filter (== '2') s
+
+main :: IO ()
+main = do
+  s <- getStr
+  putStrLn $ solver s
+
+-- read double Int score
+readDoubleIntScore :: Int -> IO [(Int, Int)]
+readDoubleIntScore n = replicateM n $ do
+  [x, y] <- map read . words <$> getLine
+  return (x, y)
+
+-- read String Array
+getStrArray :: IO [String]
+getStrArray = words <$> getLine
+
+-- read Int
+getInt :: IO Int
+getInt = readLn :: IO Int
+
+-- read String
+getStr :: IO String
+getStr = getLine
