4b98745c4604c59773479c7c831f585a7076c059
jacoloves <5511068t@gmail.com>
2025-03-05 00:46:22 +0000

    20250305

---
+++ abc329a.hs
@@ -0,0 +1,33 @@
+-- Control.Monad
+-- replicateM
+import Control.Monad (replicateM)
+
+solver :: String -> String
+solver str = unwords $ map (:[]) str
+
+main :: IO ()
+main = do
+  s <- getStr
+  putStrLn $ solver s 
+
+-- read double Int score
+readDoubleIntScore :: Int -> IO [(Int, Int)]
+readDoubleIntScore n = replicateM n $ do
+  [x, y] <- map read . words <$> getLine
+  return (x, y)
+
+-- read String Array
+getStrArray :: IO [String]
+getStrArray = words <$> getLine
+
+-- read Int
+getInt :: IO Int
+getInt = readLn :: IO Int
+
+-- read String
+getStr :: IO String
+getStr = getLine
+
+-- read Int array
+getIntArray :: IO [Int]
+getIntArray = map read . words <$> getLine :: IO [Int]
