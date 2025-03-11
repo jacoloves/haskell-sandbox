a3caf1ed70a639b7447a003c6cc07565c8724aa2
jacoloves <5511068t@gmail.com>
2025-03-11 23:57:59 +0000

    20250312

---
+++ abc323a.hs
@@ -0,0 +1,35 @@
+-- Control.Monad
+-- replicateM
+import Control.Monad (replicateM)
+
+solver :: String -> String
+solver s = if all (== '0') [s !! (i - 1) | i <- [2,4..16]]
+  then "Yes"
+  else "No"
+
+main :: IO ()
+main = do
+  s <- getStr 
+  putStrLn $ solver s 
+
+-- read double Int score
+readDoubleIntScore :: Int -> IO [(Int, Int)]
+readDoubleIntScore n = replicateM n $ do
+  [x, y] <- map read . words <$> getLine
+  return (x, y)
+
+-- read String Array
+getStrArray :: IO [String]
+getStrArray = words <$> getLine
+
+-- read Int
+getInt :: IO Int
+getInt = readLn :: IO Int
+
+-- read String
+getStr :: IO String
+getStr = getLine
+
+-- read Int array
+getIntArray :: IO [Int]
+getIntArray = map read . words <$> getLine :: IO [Int]
