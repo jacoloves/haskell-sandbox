b16cae25902f699f06684c73d1b55cef4fce6958
shooonng <5511068t@gmail.com>
2025-10-03 20:59:52 +0900

    20251003

--- playground/algogogo/october.hs
+++ playground/algogogo/october.hs
@@ -1,50 +1,85 @@
 import Control.Monad (forM_, replicateM)
 import Data.Char (isDigit, isLower, isUpper)
 import Data.Function (on)
-import Data.List (minimumBy, nub, sort, sortBy)
+import Data.List (isSuffixOf, minimumBy, nub, sort, sortBy)
 import Data.Map qualified as Map
 import Text.Printf (printf)
 
 main :: IO ()
 main = do
-  abc421a
+  abc418a
 
+abc418a :: IO ()
+abc418a = do
+  n <- getInt
+  s <- getStr
+  let ans = endsWithTea s
+  putStrLn ans
 
-abc421a :: IO()
+endsWithTea :: String -> String
+endsWithTea s
+  | "tea" `isSuffixOf` s = "Yes"
+  | otherwise = "No"
+
+abc419a :: IO ()
+abc419a = do
+  s <- getStr
+  let ans = checkAtcoderLang s
+  putStrLn ans
+
+checkAtcoderLang :: String -> String
+checkAtcoderLang s
+  | s == "red" = "SSS"
+  | s == "blue" = "FFF"
+  | s == "green" = "MMM"
+  | otherwise = "Unknown"
+
+abc420a :: IO ()
+abc420a = do
+  [x, y] <- getIntArray
+  let ans = checkXplusY x y
+  print ans
+
+checkXplusY :: Int -> Int -> Int
+checkXplusY x y =
+  let ansXY = x + y
+   in if ansXY > 12
+        then ansXY - 12
+        else ansXY
+
+abc421a :: IO ()
 abc421a = do
-    n <- getInt
-    regidents <- replicateM n getStr
-    [xStr, y] <- getStrArray
-    let x = read xStr :: Int
-    let r = checkD regidents x y
-    putStrLn r
+  n <- getInt
+  regidents <- replicateM n getStr
+  [xStr, y] <- getStrArray
+  let x = read xStr :: Int
+  let r = checkD regidents x y
+  putStrLn r
 
 checkD :: [String] -> Int -> String -> String
-checkD regidents x y = 
-    let actualResident = regidents !! (x - 1)
-    in if actualResident == y 
-        then "Yes" 
+checkD regidents x y =
+  let actualResident = regidents !! (x - 1)
+   in if actualResident == y
+        then "Yes"
         else "No"
 
-abc424a :: IO()
+abc424a :: IO ()
 abc424a = do
-    [a, b, c] <- getIntArray
-    let ans = if a == b || b == c || c == a then "Yes" else "No"
-    putStrLn ans
+  [a, b, c] <- getIntArray
+  let ans = if a == b || b == c || c == a then "Yes" else "No"
+  putStrLn ans
 
-abc425a :: IO()
+abc425a :: IO ()
 abc425a = do
-    n <- getInt
-    let r = f n
-    print r
+  n <- getInt
+  let r = f n
+  print r
 
 f :: Int -> Int
-f n = sum [f2 i | i <- [1..n] ]
+f n = sum [f2 i | i <- [1 .. n]]
 
 f2 :: Int -> Int
 f2 i = (-1) ^ i * i ^ 3
-    
-
 
 hello :: IO ()
 hello = do
